d4ebe0c21a059ad348955c930dfcb58c6dab9c93
Sharon Cherop <sh.cherop@gmail.com>
2021-10-28 23:01:56 +0300

    configuring url path for about page

--- cards/views.py
+++ cards/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request, 'all-cards/home.html')
+
+def about(request):
+    return render(request, 'all-cards/about.html')
